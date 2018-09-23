cf8019b1ef859505c6c9885ebcf4e535161cf0fc
Florence <43491397+FlorenceCobos@users.noreply.github.com>
2018-09-23 10:15:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,27 +9,27 @@ document.querySelector('body').addEventListener('keydown',function(el) {
 document.querySelector('body').addEventListener('click',function (el){
   if(el.target.classList.contains('image')){
     el.target.nextSibling.classList.toggle('hidden');
-
+    
   }
-
+  
 })
 
 function search(){
   const inputValue =
   document.querySelector('input').value;
-
-  if(inputValue === "") {
   
+  if(inputValue === "") {
+    
   } else {
     const xhttp = new XMLHttpRequest();
-  
+    
     xhttp.open("GET", "http://api.tvmaze.com/search/shows?q=" + inputValue, false);
     xhttp.send();
-  
+    
     let results = JSON.parse(xhttp.response);
     
     document.querySelector('main').innerHTML ="";
-  
+    
     for (let i = 0; i < results.length; i++) {
       displaySerie(results[i]);
     }
@@ -38,22 +38,22 @@ function search(){
 
 function displaySerie(serie) {
   let target = document.querySelector('main');
-
+  
   let div = document.createElement('DIV');
   let name = document.createElement('H2');
   let image = document.createElement('IMG');
   let status = document.createElement('P');
-
+  
   name.innerHTML = serie.show.name;
   if (serie.show.image === null) {
   }else{
-      image.src = serie.show.image.medium;
-      image.classList.add('image');
+    image.src = serie.show.image.medium;
+    image.classList.add('image');
   }
   status.innerHTML = serie.show.status;
   status.classList.add('status');
-    status.classList.add('hidden');
-    
+  status.classList.add('hidden');
+  
   div.appendChild(name);
   div.appendChild(image);
   div.appendChild(status);
